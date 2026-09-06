8b24715d331ba8cfb88e5d6dc01815a0301a83f3
uttejadabala74-maker <uttejadabala74-maker@users.noreply.github.com>
2026-09-06 19:20:50 +0530

    Auto-fix comma to dot typos in URL field

--- android/src/com/bikefinance/app/MainActivity.java
+++ android/src/com/bikefinance/app/MainActivity.java
@@ -145,6 +145,7 @@ public class MainActivity extends Activity {
             @Override
             public void onClick(DialogInterface dialog, int which) {
                 String newUrl = input.getText().toString().trim();
+                newUrl = newUrl.replace(',', '.');
                 if (!newUrl.startsWith("http://") && !newUrl.startsWith("https://")) {
                     newUrl = "http://" + newUrl;
                 }
